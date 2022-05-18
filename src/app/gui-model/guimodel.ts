aa36c823efb11c4594464a540e464558857ce027
Swissbull1 <gassmdav@students.zhaw.ch>
2022-05-18 15:43:45 +0000

    test

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "David's Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
